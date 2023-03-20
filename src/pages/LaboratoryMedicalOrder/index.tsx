1f9e6c4cef3c7299a6be4871e71a861b9dfe23cf
zz <2522643409@qq.com>
2023-03-20 09:46:14 +0800

    检验医疗单页面提交

---
+++ src/pages/LaboratoryMedicalOrder/index.tsx
@@ -0,0 +1,17 @@
+const LaboratoryMedicalOrder: React.FC = () => {
+    const access = useAccess();
+    return (
+        <PageContainer
+            ghost
+            header={{
+                title: '权限示例',
+            }}
+        >
+            <Access accessible={access.canSeeAdmin}>
+                <Button>只有 Admin 可以看到这个按钮</Button>
+            </Access>
+        </PageContainer>
+    );
+};
+
+export default AccessPage;
