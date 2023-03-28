0080bb64c16514dad4ce94eb42b7379cb71e8eb0
2522643409@qq.com <2522643409@qq.com>
2023-03-29 00:39:02 +0800

    调整代码结构并消除类型安全报错

--- src/pages/LaboratoryMedicalOrder/components/BillingDetails.tsx
+++ src/pages/LaboratoryMedicalOrder/components/BillingDetails.tsx
@@ -6,93 +6,98 @@ import ServiceItemForm from "@/pages/LaboratoryMedicalOrder/components/ServiceIt
 import styles from '../index.css';
 
 const {Text} = Typography;
+const content = <ServiceItemForm></ServiceItemForm>;
 
+// 定义表格数据类型，让表格的数据类型安全
+interface DataType {
+    key: number,
+    operate: boolean,
+    serviceItems: string,
+    quantity: number,
+    unit: string,
+    unitPrice: number,
+    amount: number,
+    isEditing: boolean
+}
 
+const initData: DataType[] = [{
+    key: 1,
+    operate: true,
+    serviceItems: '血常规(B住院)',
+    quantity: 1,
+    unit: '次',
+    unitPrice: 50,
+    amount: 50,
+    isEditing: false
+    // isEditing用来控制是否为输入框
+}, {
+    key: 2,
+    operate: true,
+    serviceItems: '血常规(C住院)',
+    quantity: 1,
+    unit: '次',
+    unitPrice: 30,
+    amount: 90,
+    isEditing: false
+}];
 
 const BillingDetails: React.FC = () => {
-    const [data, setData] = useState(initdata);
-    const content = <ServiceItemForm></ServiceItemForm>;
+    // 表格数据定义类型，当前由页面自定义，如后面需要请求后台，写到useEffect里
+    const [data, setData] = useState<DataType[]>([]);
+
     useEffect(function () {
-        setData(initdata);
+        // 后面这里改为请求后台再set值
+        setData(initData);
     }, []);
 
-    const columns: ColumnsType<any> = [
-        {
-            title: '操作',
-            key: 'operate',
-            dataIndex: 'operate',
-            //列数据在数据项中对应的路径，支持通过数组查询嵌套路径
-            render: (text, record, index) => {
-                return (
-                    <div><PlusCircleOutlined onClick={() => {
+    const columns: ColumnsType<DataType> = [{
+        title: '操作',
+        key: 'operate',
+        dataIndex: 'operate',
+        //列数据在数据项中对应的路径，支持通过数组查询嵌套路径
+        render: (text, record, index) => {
+            return (
+                <div>
+                    <PlusCircleOutlined onClick={() => {
                         addData(text, record, index)
                     }}/>
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        <DeleteOutlined onClick={() => {
-                            delValue(text, record, index)
-                        }}/>
-                    </div>
-                );
-            }
-        },
-        {
-            title: '服务项目',
-            key: 'serviceItems',
-            render: (pageD) => {
-                return (pageD.isEditing
-                    ? <Popover content={content} trigger="click">
-                        <Input/>
-                    </Popover>
-                    : <span>{pageD.serviceItems}</span>)
-            }
-        },
-        {
-            title: '数量',
-            key: 'quantity',
-            render: (pageD) => {
-                return (pageD.isEditing ? <Input/> : <span>{pageD.quantity}</span>)
-            }
-        },
-        {
-            title: '单位',
-            dataIndex: 'unit',
-            key: 'unit',
-        },
-        {
-            title: '单价',
-            dataIndex: 'unitPrice',
-            key: 'unitPrice',
-        },
-        {
-            title: '金额',
-            dataIndex: 'amount',
-            key: 'amount',
-        },
-    ];
-
-    const initdata: any = [
-        {
-            key: '1',
-            operate: true,
-            serviceItems: '血常规(B住院)',
-            quantity: 1,
-            unit: '次',
-            unitPrice: 50,
-            amount: 50,
-            isEditing: false
-            //editable用来控制是否为输入框
-        },
-        {
-            key: '2',
-            operate: true,
-            serviceItems: '血常规(C住院)',
-            quantity: 1,
-            unit: '次',
-            unitPrice: 30,
-            amount: 90,
-            isEditing: false
-        },
-    ];
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <DeleteOutlined onClick={() => {
+                        delValue(text, record, index)
+                    }}/>
+                </div>
+            );
+        }
+    }, {
+        title: '服务项目',
+        key: 'serviceItems',
+        render: (pageD) => {
+            return (
+                <>
+                    {pageD.isEditing && <Popover content={content} trigger="click"><Input/></Popover>}
+                    {!pageD.isEditing && <span>{pageD.serviceItems}</span>}
+                </>
+            )
+        }
+    }, {
+        title: '数量',
+        key: 'quantity',
+        render: (pageD) => {
+            return (pageD.isEditing ? <Input/> : <span>{pageD.quantity}</span>)
+        }
+    }, {
+        title: '单位',
+        dataIndex: 'unit',
+        key: 'unit',
+    }, {
+        title: '单价',
+        dataIndex: 'unitPrice',
+        key: 'unitPrice',
+    }, {
+        title: '金额',
+        dataIndex: 'amount',
+        key: 'amount',
+    }];
 
     const addData = (text: any, record: any, index: number) => {
         console.log(text);
@@ -107,16 +112,16 @@ const BillingDetails: React.FC = () => {
             key: (new Date()).getTime(),
             operate: true,
             serviceItems: "",
-            quantity: "",
+            quantity: 0,
             unit: "",
-            unitPrice: "",
-            amount: "",
+            unitPrice: 0,
+            amount: 0,
             isEditing: true,
         });
         setData(newData);
-
         //类型为` any[] `的参数不能赋值给类型为` SetStateAction `的参数。
     }
+
     const delValue = (text: any, record: any, index: number) => {
         console.log(text);
         console.log(record);
@@ -152,4 +157,5 @@ const BillingDetails: React.FC = () => {
         </>
     );
 };
+
 export default BillingDetails
